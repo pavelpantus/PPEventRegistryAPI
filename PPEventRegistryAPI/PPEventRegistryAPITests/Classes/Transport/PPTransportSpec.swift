8531ac4b4dfb5f023d78a2e96cad018a7b1d60ff
Pavel Pantus <pavel@usebutton.com>
2016-07-16 11:31:34 -0400

    empty PPTransport spec

---
+++ PPEventRegistryAPI/PPEventRegistryAPITests/Classes/Transport/PPTransportSpec.swift
@@ -0,0 +1,19 @@
+//
+//  PPTransportSpec.swift
+//  PPEventRegistryAPI
+//
+//  Created by Pavel Pantus on 7/16/16.
+//  Copyright © 2016 Pavel Pantus. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import PPEventRegistryAPI
+
+class PPTransportSpec: QuickSpec {
+    override func spec() {
+
+        
+
+    }
+}
