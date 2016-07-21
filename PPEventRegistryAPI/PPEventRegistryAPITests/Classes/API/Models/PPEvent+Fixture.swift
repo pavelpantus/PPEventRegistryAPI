ef75158129decc624b488178674b827c2bcf60c7
Pavel Pantus <pavel@usebutton.com>
2016-07-21 09:16:27 -0400

    #8 - cover PPModelMapper with tests

---
+++ PPEventRegistryAPI/PPEventRegistryAPITests/Classes/API/Models/PPEvent+Fixture.swift
@@ -0,0 +1,67 @@
+//
+//  PPEvent+Fixture.swift
+//  PPEventRegistryAPI
+//
+//  Created by Pavel Pantus on 7/21/16.
+//  Copyright © 2016 Pavel Pantus. All rights reserved.
+//
+
+import Foundation
+@testable import PPEventRegistryAPI
+
+extension PPEvent {
+    
+    static func fixture1() -> [String: AnyObject] {
+        return [
+            "info" : [
+                "summary" : [
+                    "eng" : "All Exxon Mobil Corp. had to do is run its Los Angeles-area refinery for 15 days before handing the keys to new owner PBF Energy.\n\nThat's turning into a struggle. More than a month after Exxon Mobil restarted a key gasoline-making unit that was damaged in a blast last year, PBF still hasn't taken control of the Torrance plant. In the latest snag, a crane fell over Monday. While Exxon says it had no effect on production and the handover to PBF is still scheduled for mid year, the incident highligh"
+                ],
+                "location" : [
+                    "type" : "place",
+                    "label" : [
+                        "eng" : "Torrance, California"
+                    ],
+                    "country" : [
+                        "type" : "country",
+                        "label" : [
+                            "eng" : "United States"
+                        ]
+                    ]
+                ],
+                "images" : [
+                    "https://assets.bwbx.io/business/public/images/social_fallbacks/bloomberg_default-a4f15fa7ee.jpg"
+                ],
+                "categories" : [
+                    [
+                        "id" : 1214,
+                        "label" : "Recreation/Outdoors/Wildlife",
+                        "wgt" : 43,
+                        "uri" : "dmoz/Recreation/Outdoors/Wildlife"
+                    ]
+                ],
+                "totalArticleCount" : 7,
+                "title" : [
+                    "eng" : "Exxon Mobil struggles to run California plant long enough to dump it"
+                ],
+                "eventDate" : "2016-06-20",
+                "articleCounts" : [
+                    "eng" : 7
+                ],
+                "uri" : "4480701",
+                "concepts" : [
+                    [
+                        "id" : "174535",
+                        "score" : 9,
+                        "label" : [
+                            "eng" : "Maxine Waters"
+                        ],
+                        "type" : "person",
+                        "uri" : "http://en.wikipedia.org/wiki/Maxine_Waters"
+                    ]
+                ]
+            ]
+        ]
+    }
+
+}
--- PPEventRegistryAPI/PPEventRegistryAPITests/Classes/API/Operations/PPGetEventOperation+Fixture.swift
+++ PPEventRegistryAPI/PPEventRegistryAPITests/Classes/API/Operations/PPGetEventOperation+Fixture.swift
@@ -17,56 +17,7 @@ extension PPGetEventOperation {
             true
         }) { (response) -> OHHTTPStubsResponse in
             let responseData : [String: AnyObject] = [
-                "4480701" : [
-                    "info" : [
-                        "summary" : [
-                            "eng" : "All Exxon Mobil Corp. had to do is run its Los Angeles-area refinery for 15 days before handing the keys to new owner PBF Energy.\n\nThat's turning into a struggle. More than a month after Exxon Mobil restarted a key gasoline-making unit that was damaged in a blast last year, PBF still hasn't taken control of the Torrance plant. In the latest snag, a crane fell over Monday. While Exxon says it had no effect on production and the handover to PBF is still scheduled for mid year, the incident highligh"
-                        ],
-                        "location" : [
-                            "type" : "place",
-                            "label" : [
-                            "eng" : "Torrance, California"
-                            ],
-                            "country" : [
-                            "type" : "country",
-                            "label" : [
-                            "eng" : "United States"
-                            ]
-                            ]
-                        ],
-                        "images" : [
-                            "https://assets.bwbx.io/business/public/images/social_fallbacks/bloomberg_default-a4f15fa7ee.jpg"
-                        ],
-                        "categories" : [
-                            [
-                                "id" : 1214,
-                                "label" : "Recreation/Outdoors/Wildlife",
-                                "wgt" : 43,
-                                "uri" : "dmoz/Recreation/Outdoors/Wildlife"
-                            ]
-                        ],
-                        "totalArticleCount" : 7,
-                        "title" : [
-                            "eng" : "Exxon Mobil struggles to run California plant long enough to dump it"
-                        ],
-                        "eventDate" : "2016-06-20",
-                        "articleCounts" : [
-                            "eng" : 7
-                        ],
-                        "uri" : "4480701",
-                        "concepts" : [
-                            [
-                                "id" : "174535",
-                                "score" : 9,
-                                "label" : [
-                                    "eng" : "Maxine Waters"
-                                ],
-                                "type" : "person",
-                                "uri" : "http://en.wikipedia.org/wiki/Maxine_Waters"
-                            ]
-                        ]
-                    ]
-                ]
+                "4480701" : PPEvent.fixture1()
             ]
             return OHHTTPStubsResponse(jsonObject: responseData, statusCode: 200, headers: nil)
             }.name = "Get Event Operation Stub: Success"
