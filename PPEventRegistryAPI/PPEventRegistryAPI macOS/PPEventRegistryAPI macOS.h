9863ad3800c210b1c9b795a01509e579075cb418
Pavel Pantus <pavel@usebutton.com>
2016-07-22 08:06:37 -0400

    #19 - add macOS target

---
+++ PPEventRegistryAPI/PPEventRegistryAPI macOS/PPEventRegistryAPI macOS.h
@@ -0,0 +1,19 @@
+//
+//  PPEventRegistryAPI macOS.h
+//  PPEventRegistryAPI macOS
+//
+//  Created by Pavel Pantus on 7/22/16.
+//  Copyright © 2016 Pavel Pantus. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PPEventRegistryAPI macOS.
+FOUNDATION_EXPORT double PPEventRegistryAPI_macOSVersionNumber;
+
+//! Project version string for PPEventRegistryAPI macOS.
+FOUNDATION_EXPORT const unsigned char PPEventRegistryAPI_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PPEventRegistryAPI_macOS/PublicHeader.h>
+
+
