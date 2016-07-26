9ca3495b0383fdba2b35cadadd49849454ff2c70
Pavel Pantus <pavel@usebutton.com>
2016-07-25 22:49:46 -0400

    #29 - Test Project: add target for every platform that uses every api call

---
+++ Example/PPEventRegistryTest macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PPEventRegistryTest macOS
+//
+//  Created by Pavel Pantus on 7/25/16.
+//  Copyright © 2016 Pavel Pantus. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Example/PPEventRegistryTest macOS/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  PPEventRegistryTest macOS
+//
+//  Created by Pavel Pantus on 7/25/16.
+//  Copyright © 2016 Pavel Pantus. All rights reserved.
+//
+
+import Cocoa
+import PPEventRegistryAPI
+
+class ViewController: NSViewController {
+
+    let eventRegistryAPI = PPEventRegistryAPI()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        eventRegistryAPI.login("", password: "") { (error) in
+            print("login with error: \(error)")
+            self.eventRegistryAPI.getEvent(withID: 4480838701, completionHandler: { (event, error) in
+                print("get event error \(event)")
+            })
+            self.eventRegistryAPI.getRecentArticles({ (aricles, error) in
+                print("articles \(aricles), error \(error)")
+            })
+        }
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
